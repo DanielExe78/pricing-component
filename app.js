25f87b5bc1eea0409b61c753b0061d01f25ed561
DanielExe78 <100711304+DanielExe78@users.noreply.github.com>
2022-06-06 09:38:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,13 +7,8 @@ const anual = document.querySelectorAll(".anual-price");
 monthly.forEach((month) => {
   anual.forEach((full) => {
     btn.addEventListener("click", function () {
-      if (!month.classList.contains("hidden")) {
-        month.classList.toggle("hidden");
-        full.classList.toggle("hidden");
-      } else {
-        full.classList.toggle("hidden");
-        month.classList.toggle("hidden");
-      }
+      month.classList.toggle("hidden");
+      full.classList.toggle("hidden");
     });
   });
 });
